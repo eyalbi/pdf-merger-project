2e0af737bf8952294dfd9d711608bfab8e745dd5
Linoy <linoyaslan@gmail.com>
2021-06-06 17:10:00 +0300

    last-changes
    Co-Authored-By: TzlilLevi <57330791+TzlilLevi@users.noreply.github.com>

--- backend/app.py
+++ backend/app.py
@@ -15,7 +15,7 @@ from bson.objectid import ObjectId
 from flask_login import LoginManager, current_user, login_user, login_required, logout_user
 from flask_principal import Principal, Permission, RoleNeed, identity_changed, identity_loaded, Identity, AnonymousIdentity, UserNeed
 
-from forms import LoginForm, RegistrationForm,AdminDeleteForm, AdminUpdateForm, AdminSendEmailForm,StudentMessage,uploadInovice,DownloadInovice
+from forms import LoginForm, RegistrationForm,uploadInovice,DownloadInovice
 from models import ROLES
 
 #linoy's code
@@ -95,27 +95,6 @@ def on_identity_loaded(sender, identity):
     if hasattr(current_user, 'role'):
         identity.provides.add(RoleNeed(current_user.role))
 
-
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     if current_user.is_authenticated:
-#         return redirect(url_for('index'))
-#     form = LoginForm()
-#     if form.validate_on_submit():
-#         user = User.objects(username=form.username.data).first()
-#         if user.Blocked == 'true' or user.Blocked == 'True':
-#             flash('Your user is blocked adress Admin')
-#             return redirect(url_for('login'))
-#         if user is None or not user.check_password(form.password.data):
-#             flash('Invalid username or password')
-#             return redirect(url_for('login'))
-#         login_user(user, remember=form.remember_me.data)
-#         identity_changed.send(current_app._get_current_object(),
-#                               identity=Identity(user.username))
-#         return redirect(url_for('index'))
-#     return render_template('login.html', title='Sign In', form=form)
-
-
 #tzlil and linoy's changes
 @app.route('/new', methods=['GET', 'POST'])
 def login():
@@ -127,9 +106,6 @@ def login():
    
     if form1.validate_on_submit():
         user = User.objects(username=form1.username.data).first()
-        #if user.Blocked == 'true' or user.Blocked == 'True':
-        #    flash('Your user is blocked adress Admin')
-        #    return redirect(url_for('login'))
         if user is None or not user.check_password(form1.password.data):
             flash('Invalid username or password')
             return redirect(url_for('login'))
@@ -179,24 +155,27 @@ def create_user(form):
     user.save()
 
 def Merge_pdf(user):
-    inovices = Inovice.objects(inovice_Customer = current_user.username)
-    counter = 0 
-    pdfWriter = PyPDF2.PdfFileWriter()
-    for i in inovices:
-        pdfFile = open(i.Inovice_pdf,'rb')
-        pdfReader = PyPDF2.PdfFileReader(pdfFile)
-        for pageNum in range(pdfReader.numPages):
-            pdfWriter.addPage(pdfReader.getPage(pageNum))
-    today = datetime.today()
-    YandM = datetime(today.year, today.month, 1)
-    timeString = str(YandM).split(" ")[0]
+    try:
+        inovices = Inovice.objects(inovice_Customer = current_user.username)
+        counter = 0 
+        pdfWriter = PyPDF2.PdfFileWriter()
+        for i in inovices:
+            pdfFile = open(i.Inovice_pdf,'rb')
+            pdfReader = PyPDF2.PdfFileReader(pdfFile)
+            for pageNum in range(pdfReader.numPages):
+                pdfWriter.addPage(pdfReader.getPage(pageNum))
+        today = datetime.today()
+        YandM = datetime(today.year, today.month, 1)
+        timeString = str(YandM).split(" ")[0]
 
-    loc = os.path.join(basedir, 'Downloads')
-    pdfOutputFile = open(os.path.join(loc, '{0}-{1}-MergedFiles.pdf'.format(current_user.username,timeString)), 'wb')
-    pdfWriter.write(pdfOutputFile)
-    pdfOutputFile.close()
-    pdfFile.close()
-    return os.path.join(loc, '{0}-{1}-MergedFiles.pdf'.format(current_user.username,timeString))
+        loc = os.path.join(basedir, 'Downloads')
+        pdfOutputFile = open(os.path.join(loc, '{0}-{1}-MergedFiles.pdf'.format(current_user.username,timeString)), 'wb')
+        pdfWriter.write(pdfOutputFile)
+        pdfOutputFile.close()
+        pdfFile.close()
+        return os.path.join(loc, '{0}-{1}-MergedFiles.pdf'.format(current_user.username,timeString))
+    except:
+        flash("Opps!! Error opening file!")
 
 def send_merged():
     if date.today().day != 1:
@@ -235,6 +214,7 @@ def index():
 @app.route('/Customer/DownloadInoviceInovices', methods=['GET','POST'])
 @login_required
 def Download_merged_pdf():
+
     u = User.objects(username=current_user.username).first()
     form = DownloadInovice()
     # try:
@@ -283,28 +263,13 @@ def Delete_Pdf():
         return redirect(url_for('Delete_Pdf'))
     return render_template('DeletePdf.html', title='DownloadInovice',user=u,inovices = inovices)
 
-@app.route('/Customer/Inovices', methods=['POST'])
-@login_required
-def Upload_inv():
-    u = User.objects(username=current_user.username).first()
-    form = uploadInovice()
-    try:
-        if form.validate_on_submit():
-            create_Inovice(form,u)
-            flash('Upload seccessfull!')
-            return redirect(url_for('index'))
-        return render_template('UploadInovice.html', title='UploadInovice', form=form,user=u)
-    except:
-        flash('cant upload document')
-        return redirect('/index')
-
 
 def checkinovice(Path,accountant,Customer):
     I = Inovice.objects(Inovice_pdf = Path, inovice_Accountant = accountant,inovice_Customer = Customer )
     if I:
         return False
     return True 
-@app.route('/Customer/test', methods=['POST', 'GET'])
+@app.route('/Customer/upload', methods=['POST', 'GET'])
 @login_required
 def upload():
     
@@ -324,9 +289,7 @@ def upload():
             pdf.save()
     return render_template('test.html', user=user, form=form)
 
-# if __name__ == '__main__':
-#    app.run(debug = True)
-   
+
 @app.route('/Customer/myProfile', methods=['POST', 'GET'])
 @login_required
 def myprofile():
--- backend/forms.py
+++ backend/forms.py
@@ -21,29 +21,6 @@ class RegistrationForm(FlaskForm):
     avatar = SelectField('select avatar:', choices = [('https://robohash.org/3EC.png?set=set4','cat1'),('https://robohash.org/293.png?set=set4','cat2'),('https://robohash.org/ZOB.png?set=set4','cat3')])
     submit2 = SubmitField('Sign Up')
 
-class AdminDeleteForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    submit = SubmitField('Delete')
-
-class AdminBlockForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    submit = SubmitField('Block user')
-   
-class AdminUpdateForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    first_name = StringField('First Name', validators=[DataRequired()])
-    last_name = StringField('Last Name', validators=[DataRequired()])
-    submit = SubmitField('update')
-
-class AdminSendEmailForm(FlaskForm):
-    message = StringField('Message', validators=[DataRequired()])
-    submit = SubmitField('Send Message')
-
-class StudentMessage(FlaskForm):
-    lecturer = StringField('lecturer', validators=[DataRequired()])
-    message = StringField('Message', validators=[DataRequired()])
-    submit = SubmitField('Send Message')
-
 class uploadInovice(FlaskForm):
     Inovice_pdf = StringField(default='')
     inovice_Customer = StringField(default='')
@@ -52,22 +29,3 @@ class uploadInovice(FlaskForm):
 
 class DownloadInovice(FlaskForm):
     submit = SubmitField('Download Merged PDf')
-
-# class AdminUpdateForm(FlaskForm):
-#     username = StringField('Username', validators=[DataRequired()])
-#     email = StringField('Email', validators=[DataRequired(), Email()])
-#     password = PasswordField('Password', validators=[DataRequired()])
-#     password2 = PasswordField(
-#         'Repeat Password', validators=[DataRequired(), EqualTo('password')])
-#     first_name = StringField('First Name', validators=[DataRequired()])
-#     last_name = StringField('Last Name', validators=[DataRequired()])
-   
-    # def validate_username(self, username):
-    #     user = User.query.filter_by(username=username.data).first()
-    #     if user is not None:
-    #         raise ValidationError('Please use a different username.')
-
-    # def validate_email(self, email):
-    #     user = User.query.filter_by(email=email.data).first()
-    #     if user is not None:
-    #         raise ValidationError('Please use a different email address.')
